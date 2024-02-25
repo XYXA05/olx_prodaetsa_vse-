be88bfcc2c30430a4bb46021cf3a56941794ae36
XYXA05 <110311275+XYXA05@users.noreply.github.com>
2024-02-26 00:01:25 +0100

    Update database.py

--- backand/database.py
+++ backand/database.py
@@ -2,7 +2,7 @@ import certifi
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb+srv://bogdansavi05:iAcMgXdOvohD5ly3@olx.4p6lijm.mongodb.net/', tlsCAFile=certifi.where())
+client = MongoClient('******************************', tlsCAFile=certifi.where())
 db = client.todo_db
 
 collection_name = db['items_olx']   
